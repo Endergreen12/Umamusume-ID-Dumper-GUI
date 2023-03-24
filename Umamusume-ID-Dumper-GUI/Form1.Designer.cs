3837c647b2e994cbc677792f10d72b0a35ec10ab
Endergreen12 <endergreenjapan@gmail.com>
2023-03-24 20:01:28 +0900

    プロジェクト ファイルを追加します。

---
+++ Umamusume-ID-Dumper-GUI/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Umamusume_ID_Dumper_GUI
+{
+    partial class app
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(app));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnGetData = new System.Windows.Forms.Button();
+            this.cmbBoxData = new System.Windows.Forms.ComboBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnSaveToFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.name});
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // id
+            // 
+            this.id.FillWeight = 20.30457F;
+            resources.ApplyResources(this.id, "id");
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
+            // name
+            // 
+            this.name.FillWeight = 179.6954F;
+            resources.ApplyResources(this.name, "name");
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            // 
+            // btnGetData
+            // 
+            resources.ApplyResources(this.btnGetData, "btnGetData");
+            this.btnGetData.Name = "btnGetData";
+            this.btnGetData.UseVisualStyleBackColor = true;
+            this.btnGetData.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmbBoxData
+            // 
+            resources.ApplyResources(this.cmbBoxData, "cmbBoxData");
+            this.cmbBoxData.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBoxData.FormattingEnabled = true;
+            this.cmbBoxData.Items.AddRange(new object[] {
+            resources.GetString("cmbBoxData.Items"),
+            resources.GetString("cmbBoxData.Items1"),
+            resources.GetString("cmbBoxData.Items2")});
+            this.cmbBoxData.Name = "cmbBoxData";
+            // 
+            // lblStatus
+            // 
+            resources.ApplyResources(this.lblStatus, "lblStatus");
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnSaveToFile
+            // 
+            resources.ApplyResources(this.btnSaveToFile, "btnSaveToFile");
+            this.btnSaveToFile.Name = "btnSaveToFile";
+            this.btnSaveToFile.UseVisualStyleBackColor = true;
+            this.btnSaveToFile.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // app
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnSaveToFile);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.cmbBoxData);
+            this.Controls.Add(this.btnGetData);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "app";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnGetData;
+        private System.Windows.Forms.ComboBox cmbBoxData;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.Button btnSaveToFile;
+    }
+}
+
---
+++ Umamusume-ID-Dumper-GUI/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.Sqlite;
+using Umamusume_ID_Dumper_GUI.Properties;
+
+namespace Umamusume_ID_Dumper_GUI
+{
+    public partial class app : Form
+    {
+        private string dbPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "Low\\Cygames\\umamusume\\master\\master.mdb";
+        private SqliteConnection conn = null;
+        private SqliteCommand cmd = null;
+        private string[] targetField = new string[] { "index", "text" };
+
+        public app()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InitializeConnection();
+
+            if (cmd != null)
+            {
+                UpdateStatus(2);
+
+                conn.Open();
+
+                if(cmbBoxData.SelectedIndex < 0)
+                {
+                    UpdateStatus(5);
+                    return;
+                }
+
+                UpdateStatus(3);
+
+                dataGridView1.Rows.Clear();
+                using (SqliteDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        dataGridView1.Rows.Add(reader.GetString(0), reader.GetString(1));
+                    }
+                }
+
+                conn.Close();
+
+                UpdateStatus(4);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateStatus(0);
+            cmbBoxData.SelectedIndex = 0;
+        }
+
+        private int GetDataCategoryInt(int category)
+        {
+            int data = -1;
+
+            switch(category)
+            {
+                case 0: // Character
+                    data = 6;
+                    break;
+
+                case 1: // Dress
+                    data = 14;
+                    break;
+
+                case 2: // Live
+                    data = 16;
+                    break;
+            }
+
+            return data;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Status text is updated according to the value of the argument status.
+        /// </summary>
+        /// <param name="status">
+        /// 0: Initial state<br />
+        /// 1: Verifying that the database exists<br />
+        /// 2: Opening the database<br />
+        /// 3: Getting data<br />
+        /// 4: Done<br />
+        /// 5: An error has occurred
+        /// </param>
+        /// <param name="errorIndex">
+        /// 0: Database existence could not be verified
+        /// </param>
+        private void UpdateStatus(int status = -1, int errorIndex = -1)
+        {
+            string statusTxt = "";
+
+            switch(status)
+            {
+                case 0:
+                    statusTxt = Resources.status0;
+                    break;
+
+                case 1:
+                    statusTxt = Resources.status1;
+                    break;
+
+                case 2:
+                    statusTxt = Resources.status2;
+                    break;
+
+                case 3:
+                    statusTxt = Resources.status3;
+                    break;
+
+                case 4:
+                    statusTxt = Resources.status4;
+                    break;
+
+                case 5:
+                    var errorTxt = "";
+
+                    switch(errorIndex)
+                    {
+                        case 0:
+                            errorTxt = Resources.error0;
+                            break;
+                    }
+
+                    statusTxt = Resources.status5 + ": " + errorTxt;
+                    break;
+            }
+
+            lblStatus.Text = Resources.status + ": " + statusTxt;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            InitializeConnection();
+
+            if (conn == null || cmd == null)
+                return;
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.FileName = cmbBoxData.Text + ".txt";
+            saveFileDialog.Title = "";
+            saveFileDialog.Filter = "テキストファイル(*.txt)|*.txt";
+
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Stream stream;
+                stream = saveFileDialog.OpenFile();
+                if(stream != null)
+                {
+                    StreamWriter streamWriter = new StreamWriter(stream);
+
+                    var dumpedText = "";
+
+                    UpdateStatus(2);
+                    conn.Open();
+                    using (SqliteDataReader reader = cmd.ExecuteReader())
+                    {
+                        UpdateStatus(3);
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < targetField.Length; i++)
+                            {
+                                dumpedText += reader.GetString(i);
+                                if (i == targetField.Length - 1)
+                                    dumpedText += "\r\n";
+                                else
+                                    dumpedText += "\t";
+                            }
+                        }
+                    }
+
+                    streamWriter.Write(dumpedText);
+                    streamWriter.Flush();
+                    stream.Close();
+                    conn.Close();
+
+                    UpdateStatus(4);
+                }
+            }
+        }
+
+        private void InitializeConnection()
+        {
+            UpdateStatus(1);
+
+            if (File.Exists(dbPath))
+            {
+                conn = new($"Data Source={dbPath}");
+
+                cmd = conn.CreateCommand();
+
+                var tableName = "text_data";
+                var targetFieldStr = @"""" + String.Join(@""",""", targetField) + @"""";
+                var refinementField = "category";
+                var refinementNum = GetDataCategoryInt(cmbBoxData.SelectedIndex);
+
+                cmd.CommandText =
+                        $@" SELECT {targetFieldStr}
+                            FROM {tableName}
+                        ";
+
+                if (refinementField != "")
+                    cmd.CommandText += $@" WHERE {refinementField} = {refinementNum}";
+            }
+            else
+            {
+                UpdateStatus(5, 0);
+            }
+        }
+    }
+}
